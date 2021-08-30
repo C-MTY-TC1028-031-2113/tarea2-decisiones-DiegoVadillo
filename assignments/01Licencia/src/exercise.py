d17143c58bcc1e0a534b2794056785ff556d4992
Your Name <you@example.com>
2021-08-29 20:22:49 -0500

    20210829

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,12 @@
 
 def main():
-    pass
-
+    edad= int(input("cual es tu edad:" ))
+    if edad >= 18:
+        ident=str(input("¿Tienes identificación oficial? (s/n):"))
+        if ident == "s" : 
+            print("Tramite de licencia concedido")
+    else: 
+        print("no cumples los requisitos")
 
 if __name__ == '__main__':
     main()
