cbc80498fa2e222dcfd71903c95a055a91d013be
Your Name <you@example.com>
2021-08-30 00:07:46 -0500

    20210829

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,8 +3,9 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    
-    print("Hola")
+    if x<y and x<z and y<z:
+        print(float(x))
+        
 
 if __name__=='__main__':
     main()
--- assignments/04Cuadrante/src/exercise.py
+++ assignments/04Cuadrante/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    numero= int(input("dame un numero"))
+    if numero == 360 or 270 or 180 or 90 or 0:
+        print ("eje")
+    elif numero > 0 and < 90:
+        print ("cuandrante 1")
+    elif numero > 90 and < 180:
+        print("cuadrante 2")
+    elif numero > 180 and < 270:
+        print("cuadrante 3")
+    elif numero > 270 and < 360:
+        print("cuadrante 4")
+    elif numero >360: 
+        print("excede")
 
 if __name__ == '__main__':
     main()
